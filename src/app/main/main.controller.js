56ab6791110e43a7bb27b62bfe624642e288fdad
juno <juno@krasava.fi>
2015-01-20 05:43:32 +0200

    prod

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,13 +1,30 @@
 'use strict';
 
 angular.module('fiFifortuneDesk0')
-  .controller('MainCtrl', function ($scope,$location,zodiacmakerService,jsonpService) {
+  .controller('MainCtrl', function ($scope,$location,jsonpService) {
 	  
-		var allzodiacs =zodiacmakerService.getZodiacs();	    	    
-	    var rand = Math.floor(Math.random() *allzodiacs.length);
-	    
-	    $scope.zodiac = allzodiacs[rand];
+//		var allzodiacs =zodiacmakerService.getZodiacs();	    	    
+//	    var rand = Math.floor(Math.random() *allzodiacs.length);	    
+//	    $scope.zodiac = allzodiacs[rand];
+
+	  
+	  
+	  var allfortunetellers = function() {
+		  
+		  jsonpService.getJSON('http://'+$location.host()+':80/fortunetellers').then(function(data) {
+			  
+			  
+			  $scope.allfortunetellers =data;
+			  
+		  },function(error) {
+			  
+		  });
+		  
+		  
+	  };
+	  allfortunetellers();
+	  
 	    	    
-	    var allfortunetellers = jsonpService.getJSON('http://'+$location.host()+':80/fortunetellers');
+//	  	$scope.allfortunetellers = jsonpService.getJSON('http://'+$location.host()+':80/fortunetellers');
 	    	    	          
   });
